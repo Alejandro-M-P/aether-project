1abc0314a98a90ba4087da42d410453d096ac601
Alejandro-M-P <martinpujaltea@icloud.com>
2025-12-10 18:29:48 +0100

    final

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Poniendo las claves directamente aseguramos que no falle
 const firebaseConfig = {
-	apiKey: "AIzaSyCqT7j462Ui4QVogup6RGym-g2iHHer4qE",
-	authDomain: "aether-alejandro-9b32b.firebaseapp.com",
-	projectId: "aether-alejandro-9b32b",
-	storageBucket: "aether-alejandro-9b32b.firebasestorage.app",
-	messagingSenderId: "183118372728",
-	appId: "1:183118372728:web:751c92bb7cd8f55857728d",
+    apiKey: "AIzaSyCqT7j462Ui4QVogup6RGym-g2iHHer4qE",
+    authDomain: "aether-alejandro-9b32b.firebaseapp.com",
+    projectId: "aether-alejandro-9b32b",
+    storageBucket: "aether-alejandro-9b32b.firebasestorage.app",
+    messagingSenderId: "183118372728",
+    appId: "1:183118372728:web:751c92bb7cd8f55857728d",
 };
 
-const app = initializeApp(firebaseConfig);
+// Esta línea evita el error de "Duplicate App"
+// Si ya existe una app, la usa; si no, la crea.
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
